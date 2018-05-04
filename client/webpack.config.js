4a5c92f215bb94a849de351110c2bffcc5479cf3
user1990 <paulius.rimg1990@gmail.com>
2018-05-04 14:31:30 +0300

    Fixing build path

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -4,7 +4,7 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 module.exports = {
   entry: ['babel-polyfill', './src/index.js'],
   output: {
-    path: path.resolve(__dirname, 'client/build'),
+    path: path.resolve(__dirname, 'build'),
     filename: 'bundle.js',
   },
   plugins: [
